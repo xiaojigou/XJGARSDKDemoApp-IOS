ddf496bd798c52c7393e3bfdf289203575f2387c
TeacherLuo <14625354@qq.com>
2018-10-22 12:24:10 +0800

    delete the cached xcode directory
    Cmake can not generate correct xcode project, because cmakecache created in xcode dir has a wrong source configure. Deleting the cached xcode directory solved the problem.

--- XJGARSDKDemoApp-ios/Xcode/CMakeFiles/3.9.6/CompilerIdCXX/CompilerIdCXX.build/Debug-iphoneos/CompilerIdCXX.build/Objects-normal/armv7/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jianxinluo/Desktop/Test/XJGARSDKDemoApp-ios/Xcode/CMakeFiles/3.9.6/CompilerIdCXX/CMakeCXXCompilerId.cpp
--- XJGARSDKDemoApp-ios/Xcode/XJGARSDKDemoApp.build/Debug-iphoneos/ALL_BUILD.build/Script-865ECAA8C24240B4B032D226.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/jianxinluo/Desktop/Test/XJGARSDKDemoApp-ios/Xcode -f /Users/jianxinluo/Desktop/Test/XJGARSDKDemoApp-ios/Xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
